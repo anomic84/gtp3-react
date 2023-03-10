61a4fd3964076bf80b86438dd4fdcf21a3ac835e
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-10 14:01:50 -0800

    made and linked containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Article, Brand, CTA, Feature, Navbar } from './components'
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
 const App = () => {
   return (
     <div>
-        <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
     </div>
   )
 }
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { defualt as Blog } from './blog/Blog';
+export { defualt as Feature } from './feature/Feature';
+export { defualt as Footer } from './footer/Footer';
+export { defualt as Header } from './header/Header';
+export { defualt as Possibility } from './possibility/Possibility';
+export { defualt as WhatGPT3 } from './whatGPT3/WhatGPT3';
